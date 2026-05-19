eebd8c8237c4bfe0bf536f9574a2059d2929fdc9
DasEd955 <info@tunerdesign.org>
2026-05-18 21:29:38 -0400

    Update Training loop (Training 1/4)

--- foundations/training_loop.py
+++ foundations/training_loop.py
@@ -15,9 +15,23 @@ class Solution:
         # Initialize w = zeros, b = 0
         # return (np.round(w, 5), round(b, 5))
 
+        # Initialize:
+            # w -> Learned weight vector; initially a 1D array of zeros of size X.shape[1]
+            # b -> Bias
+            # n -> Size of the dataset (rows) 
         w, b = np.zeros(X.shape[1]), 0.0
         n = X.shape[0]
 
+        # Iterate through all training epochs
+            # Model (Forward Pass): y_hat = X @ w + b
+            # Use Mean Squared Error Loss: (1/n) * np.sum(np.square(y_hat - y))
+            # Gradients:
+                # Compute error vector to simplify: y_hat - y
+                # dL_dw = (2/n) * X.T @ (error)
+                # dL_db = (2/n) * np.sum(error)
+            # Update:
+                # w -= lr * dL_dw
+                # b -= lr * dL_db
         for epoch in range(epochs):
             y_hat = X @ w + b
             mse_loss = (1/n) * np.sum(np.square(y_hat - y))
@@ -27,4 +41,5 @@ class Solution:
             w -= lr * dL_dw
             b -= lr * dL_db
 
+        # Return the output as a tuple (w, b), the learned weight vector & bias
         return (np.round(w, 5), round(b, 5))
