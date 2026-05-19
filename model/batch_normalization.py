6096d0d40933e60332b6e9f944c0ea8312fa830c
DasEd955 <info@tunerdesign.org>
2026-05-18 20:48:00 -0400

    Update Batch normalization (PyTorch 3/4)

--- model/batch_normalization.py
+++ model/batch_normalization.py
@@ -10,21 +10,33 @@ class Solution:
         # During inference: normalize using running stats (no batch stats needed)
         # Apply affine transform: y = gamma * x_hat + beta
         # Return (y, running_mean, running_var), all rounded to 4 decimals as lists
+        
+        # Convert x, gamma, beta, running_mean, running_var into NumPy arrays
         x = np.array(x)
         gamma = np.array(gamma)
         beta = np.array(beta)
         running_mean = np.array(running_mean, dtype=np.float64)
         running_var = np.array(running_var, dtype=np.float64)
 
+        # Training Mode:
+            # Compute batch_mean, batch_var, x_hat
+            # Update thr unning statistics for later use in inference (running_mean & running_var) 
         if training:
             batch_mean = np.mean(x, axis=0)
             batch_var = np.var(x, axis=0)
             x_hat = (x - batch_mean) / np.sqrt(batch_var + eps)
             running_mean = (1 - momentum) * running_mean + (momentum * batch_mean)
             running_var = (1 - momentum) * running_var + (momentum * batch_var)
+        # Inference Mode:
+            # Use the running statistics instead of batch statistics
         else:
             x_hat = (x - running_mean) / np.sqrt(running_var + eps)
         
+        # Compute y_out
         y_out = gamma * x_hat + beta
 
+        # Output: A tuple of 3 values
+            # 1. The normalized output as a 2D list (y_out)
+            # 2. The updated running_mean as a 1D list
+            # 3. The updated running_var as a 1D list
         return (np.round(y_out, 4), np.round(running_mean, 4), np.round(running_var, 4))
