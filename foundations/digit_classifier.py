e691da75131a505a8bca2c313fbc4049e932271d
DasEd955 <info@tunerdesign.org>
2026-05-20 23:59:51 -0400

    Update Digit classifier (Training 4/4)

--- foundations/digit_classifier.py
+++ foundations/digit_classifier.py
@@ -7,6 +7,11 @@ class Solution(nn.Module):
         super().__init__()
         torch.manual_seed(0)
         # Architecture: Linear(784, 512) -> ReLU -> Dropout(0.2) -> Linear(512, 10) -> Sigmoid
+            # Input(784) -> The raw pixels arrive as a flat 784-element vector (28x28)
+            # Linear(512) -> The hidden layer projects to 512 features
+            # ReLU -> Introduces nonlinearity
+            # Dropout(0.2) -> Dropout (20%) regularizes
+            # Linear(10) & Sigmoid() -> The final 10 sigmoid outputs represent the confidence that the image shows that digit (0-9)
         self.first_linear = nn.Linear(784, 512)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.2)
